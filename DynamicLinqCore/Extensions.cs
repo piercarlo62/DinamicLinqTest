e9b6d23a44a0e54691462029c6f8c57bef8a5ae4
piercarlo schiavo <piercarlo.schiavo@gmail.com>
2020-09-20 19:06:44 +0200

    Ultime Modifiche

--- DynamicLinqCore/Extensions.cs
+++ DynamicLinqCore/Extensions.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+<<<<<<< HEAD
+using System.Text.RegularExpressions;
+=======
+>>>>>>> bb8c64620c7ba8e76e6688df6ab6424a60f5d42d
 using System.Threading.Tasks;
 
 namespace DynamicLinqCore
@@ -17,5 +21,64 @@ namespace DynamicLinqCore
             }
             return string.Empty;
         }
+<<<<<<< HEAD
+
+        public static string FormatValueAsProperty(this string strValue, bool AddDesc)
+        {
+            if (strValue.Contains(" desc"))
+                strValue = strValue.Replace(" desc", "");
+            if (AddDesc)
+            {
+                return Regex.Replace(strValue, "[\\s-_!+\"'%/=()\\[\\]{}<>,;:*~ˇ^˘°\\\\#&@]", "").StripAccent().AddDesc();
+            }
+            else
+                return Regex.Replace(strValue, "[\\s-_!+\"'%/=()\\[\\]{}<>,;:*~ˇ^˘°\\\\#&@]", "").StripAccent();
+        }
+
+        public static string StripAccent(this string strValue)
+        {
+            string A;
+            string B;
+            int i;
+            const string AccChars = "ŠŽšžŸÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðñòóôõöùúûüýÿ";
+            const string RegChars = "SZszYAAAAAACEEEEIIIIDNOOOOOUUUUYaaaaaaceeeeiiiidnooooouuuuyy";
+            for (i = 0; i < AccChars.Length; i++)
+            {
+                A = AccChars.Substring(i, 1);
+                B = RegChars.Substring(i, 1);
+                strValue = strValue.Replace(A, B);
+            }
+
+            return strValue;
+        }
+
+        public static string AddDesc(this string strValue)
+        {
+            return strValue + " desc";
+        }
+    }
+
+    public static class ListExtensions
+    {
+        public static List<string> GetFormattedValueAsAPropertyList(this List<string> list)
+        {
+            List<string> listToReturn = new List<string>();
+            foreach (var item in list)
+            {
+                if (item.Contains(" desc"))
+                {
+                    var str = item.Replace(" desc", "");
+                    listToReturn.Add(str.FormatValueAsProperty(true));
+                }
+                else
+                {
+                    listToReturn.Add(item.FormatValueAsProperty(false));
+                }
+            }
+
+            return listToReturn;
+        }
+=======
+>>>>>>> bb8c64620c7ba8e76e6688df6ab6424a60f5d42d
     }
 }
--- DynamicLinqCore/Program.cs
+++ DynamicLinqCore/Program.cs
@@ -1,8 +1,15 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+<<<<<<< HEAD
+using System.Dynamic;
 using System.IO;
 using System.Linq;
+using System.Linq.Dynamic.Core;
+=======
+using System.IO;
+using System.Linq;
+>>>>>>> bb8c64620c7ba8e76e6688df6ab6424a60f5d42d
 
 namespace DynamicLinqCore
 {
@@ -14,6 +21,26 @@ namespace DynamicLinqCore
             string json = File.ReadAllText(filePath);
             var playerList = JsonConvert.DeserializeObject<List<WorkSheet>>(json);
 
+<<<<<<< HEAD
+            List<string> orderProperties = new List<string>
+            {
+                "Indirizzo - Provincia" + " desc",
+                "Indirizzo - Città"
+            };
+            string ordeByString = string.Join(",", orderProperties.GetFormattedValueAsAPropertyList());
+
+            var wsOrdered = playerList.Select(ws =>
+            {
+                IDictionary<string, object> expando = new ExpandoObject();
+                foreach (var item in orderProperties)
+                {
+                    expando[item.FormatValueAsProperty(false)] = ws.Fields.GetPropertyValue(item);
+                }
+                expando["ws"] = ws;
+                dynamic d = expando;
+                return d;
+            }).AsQueryable().OrderBy(ordeByString).Select("ws").ToDynamicList<WorkSheet>();
+=======
             var wsOrdered = playerList.AsQueryable()
                     .Select(ws => new
                     {
@@ -22,6 +49,7 @@ namespace DynamicLinqCore
                         workSheet = ws
                     })
                     .OrderBy(x => x.provincia).ThenBy(x => x.citta).Select(x => x.workSheet).ToList();
+>>>>>>> bb8c64620c7ba8e76e6688df6ab6424a60f5d42d
 
             foreach (var item in wsOrdered)
             {
